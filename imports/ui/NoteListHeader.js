4b04d422eb6d472d9ebf348ce20d2732dd7f66e6
Filip Fabianek <rosiwerk@gmail.com>
2017-07-31 15:52:16 +0200

    Another button change, very small.

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -8,7 +8,7 @@ import PropTypes from "prop-types";
 export const NoteListHeader = (props) => {
     return (
         <div className="item-list__header">
-            <button className="button" onClick={() => {
+            <button className="button button--header" onClick={() => {
                 props.meteorCall("notes.insert", (err, res) => {
                     if (res) {
                         props.Session.set("selectedNoteId", res);
